e304b27759b915ac98b0c4146aa238ba5486dc45
siva <sivanandpanda@gmail.com>
2021-03-01 23:50:03 +0800

    binary search tree insert, get, min, max, delete, traversal

---
+++ src/main/java/ds/bst/Tree.java
@@ -0,0 +1,87 @@
+package ds.bst;
+
+public class Tree {
+
+    private TreeNode root;
+
+    public void insert(int value) {
+        if(root == null) {
+            root = new TreeNode(value);
+        } else {
+            root.insert(value);
+        }
+    }
+
+    public TreeNode get(int value) {
+        if(root != null) {
+            return root.get(value);
+        }
+
+        return null;
+    }
+
+    public void delete(int value) {
+        root = delete(root, value);
+    }
+
+    private TreeNode delete(TreeNode subTreeRoot, int value) {
+        if (subTreeRoot == null) {
+            return subTreeRoot;
+        }
+
+        if(value < subTreeRoot.getData()) {
+            subTreeRoot.setLeftChild(delete(subTreeRoot.getLeftChild(), value));
+        } else if(value > subTreeRoot.getData()) {
+            subTreeRoot.setRightChild(delete(subTreeRoot.getRightChild(), value));
+        } else {
+            if(subTreeRoot.getLeftChild() == null) {
+                return subTreeRoot.getRightChild(); //cases 1 & 2: works for cases when no child or one chile
+            } else if(subTreeRoot.getRightChild() == null) {
+                return subTreeRoot.getLeftChild(); //cases 1 & 2: works for cases when no child or one chile
+            } else { //case 3, both children are present
+
+                //replace the value in the subtreeRoot node with the smallest value from the right subtree
+                subTreeRoot.setData(subTreeRoot.getRightChild().min());
+
+                //delete the node that has the smallest value in the right subtree
+                subTreeRoot.setRightChild(delete(subTreeRoot.getRightChild(), subTreeRoot.getData()));
+            }
+        }
+
+        return subTreeRoot;
+    }
+
+    public int min() {
+        if(root != null) {
+            return root.min();
+        }
+
+        return Integer.MIN_VALUE;
+    }
+
+    public int max() {
+        if(root != null) {
+            return root.max();
+        }
+
+        return Integer.MAX_VALUE;
+    }
+
+    public void traverseInOrder() {
+        if(root != null) {
+            root.traverseInOrder();
+        }
+    }
+
+    public void traversePreOrder() {
+        if(root != null) {
+            root.traversePreOrder();
+        }
+    }
+
+    public void traversePostOrder() {
+        if(root != null) {
+            root.traversePostOrder();
+        }
+    }
+}
---
+++ src/test/java/ds/bst/TreeTest.java
@@ -0,0 +1,125 @@
+package ds.bst;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TreeTest {
+
+    @Test
+    void test_insert_and_traverse_inorder() {
+        Tree intTree = createIntTree();
+
+        intTree.traverseInOrder();
+        System.out.println();
+    }
+
+    @Test
+    void test_insert_and_find() {
+        Tree intTree = createIntTree();
+
+        System.out.println(intTree.get(26));
+        System.out.println(intTree.get(32));
+        System.out.println(intTree.get(23));
+        assertNotNull(intTree.get(26));
+        assertNotNull(intTree.get(32));
+        assertNull(intTree.get(23));
+    }
+
+    @Test
+    void test_insert_and_delete_root() {
+        Tree intTree = createIntTree();
+        intTree.traverseInOrder();
+        System.out.println();
+
+        intTree.delete(25);
+
+        intTree.traverseInOrder();
+        System.out.println();
+    }
+
+    @Test
+    void test_insert_and_delete_1() {
+        Tree intTree = createIntTree();
+        intTree.insert(17);
+        intTree.traverseInOrder();
+        System.out.println();
+
+        intTree.delete(17);
+
+        intTree.traverseInOrder();
+        System.out.println();
+    }
+
+    @Test
+    void test_insert_and_delete_2() {
+        Tree intTree = createIntTree();
+        intTree.insert(17);
+        intTree.traverseInOrder();
+        System.out.println();
+
+        intTree.delete(15);
+
+        intTree.traverseInOrder();
+        System.out.println();
+    }
+
+    @Test
+    void test_insert_and_delete_3() {
+        Tree intTree = createIntTree();
+        intTree.traverseInOrder();
+        System.out.println();
+
+        intTree.delete(888);
+
+        intTree.traverseInOrder();
+        System.out.println();
+    }
+
+    @Test
+    void test_insert_and_find_min() {
+        Tree intTree = createIntTree();
+
+        System.out.println(intTree.min());
+        assertEquals(15, intTree.min());
+    }
+
+    @Test
+    void test_insert_and_find_max() {
+        Tree intTree = createIntTree();
+
+        System.out.println(intTree.max());
+        assertEquals(32, intTree.max());
+    }
+
+    @Test
+    void test_insert_and_traverse_preorder() {
+        Tree intTree = createIntTree();
+
+        intTree.traversePreOrder();
+        System.out.println();
+    }
+
+    @Test
+    void test_insert_and_traverse_postorder() {
+        Tree intTree = createIntTree();
+
+        intTree.traversePostOrder();
+        System.out.println();
+    }
+
+    private Tree createIntTree() {
+        Tree intTree = new Tree();
+        intTree.insert(25);
+        intTree.insert(20);
+        intTree.insert(15);
+        intTree.insert(27);
+        intTree.insert(30);
+        intTree.insert(29);
+        intTree.insert(26);
+        intTree.insert(22);
+        intTree.insert(32);
+        return intTree;
+    }
+
+}
