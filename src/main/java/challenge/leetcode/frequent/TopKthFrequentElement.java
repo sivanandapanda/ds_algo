0c8457e2752d892ea5b4f643def6a4ec4233e1f9
sivananda.panda <sivananda.panda1@gmail.com>
2023-03-24 21:18:17 +0800

    leetcode 49 - bucket sort solution (not working)

--- src/main/java/challenge/leetcode/frequent/TopKthFrequentElement.java
+++ src/main/java/challenge/leetcode/frequent/TopKthFrequentElement.java
@@ -30,5 +30,41 @@ public class TopKthFrequentElement {
 
         return result;
     }
+    
+    //below solution is also known as bucket sort
+    //time complexity = O(n + n) (i.e. linear time to populate the map and then linear time to populate the result array)
+    //space complexity = O(n) + O(k) (i.e. space for new hashMap + result array)
+    int[] betterSolution(int[] nums, int k){
+        var numberCountMap = new HashMap<Integer, Integer>();
+        for (int currentNumber : nums) {
+            var count = numberCountMap.getOrDefault(currentNumber, 0) + 1;
+            numberCountMap.put(currentNumber, count);
+        }
+
+        var bucket = new List[k];
+        numberCountMap.forEach((key,value) -> {
+            var listOfItems = bucket[value] != null ? bucket[value] : new ArrayList<Integer>();
+            listOfItems.add(key);
+            bucket[value] = listOfItems;
+        });
+
+        var result = new int[k];
+
+        int resultsFound = 0;
+        for (int i = k-1; i >= 0; i--) {
+            if (bucket[i] == null) continue;
+            for (Object fromBucket: bucket[i]) {
+                if (fromBucket instanceof Integer) {
+                    result[resultsFound++] = (int) fromBucket;
+
+                    if(resultsFound == k) {
+                        return result;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
 
 }
